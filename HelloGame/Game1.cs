ab82d3a7385a63c390a4e6e2e0e765b659947914
yoshkinsch <clkinsch@ksu.edu>
2025-08-26 12:17:50 -0500

    HelloGame Tutorial Finished

--- HelloGame/Game1.cs
+++ HelloGame/Game1.cs
@@ -8,6 +8,9 @@ namespace HelloGame
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Texture2D _ballTexture;
+        private Vector2 _ballPosition;
+        private Vector2 _ballVelocity;
 
         public Game1()
         {
@@ -19,6 +22,12 @@ namespace HelloGame
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            _ballPosition = new Vector2((_graphics.GraphicsDevice.Viewport.Width / 2) - 32, (_graphics.GraphicsDevice.Viewport.Height / 2) - 32);
+
+            System.Random rand = new System.Random();
+            _ballVelocity = new Vector2((float)rand.NextDouble(), (float)rand.NextDouble());
+            _ballVelocity.Normalize();
+            _ballVelocity *= 100;
 
             base.Initialize();
         }
@@ -28,6 +37,7 @@ namespace HelloGame
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            _ballTexture = Content.Load<Texture2D>("Ball");
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,6 +46,17 @@ namespace HelloGame
                 Exit();
 
             // TODO: Add your update logic here
+            _ballPosition += _ballVelocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if(_ballPosition.X < _graphics.GraphicsDevice.Viewport.X || _ballPosition.X > _graphics.GraphicsDevice.Viewport.Width - 64)
+            {
+                _ballVelocity.X *= -1;
+            }
+
+            if (_ballPosition.Y < _graphics.GraphicsDevice.Viewport.Y || _ballPosition.Y > _graphics.GraphicsDevice.Viewport.Height - 64)
+            {
+                _ballVelocity.Y *= -1;
+            }
 
             base.Update(gameTime);
         }
@@ -45,6 +66,9 @@ namespace HelloGame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_ballTexture, _ballPosition, Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
